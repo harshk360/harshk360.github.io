65bb453234566e7d79acd61f3c8b46eabdcfded0
Harshit Kumar <harshk360@gmail.com>
2016-10-02 18:10:16 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '/assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js-two', '/assets/particles_two.json', function() {
+    console.log('callback - particles.js config loaded');
+});
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js-background', '/assets/particles_background.json', function() {
+    console.log('callback - particles.js config loaded');
+});
